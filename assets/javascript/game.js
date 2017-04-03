30efcbf0cbbda9fcdf55d64b36e64b1e977741ce
Daniel Walden <Ariel@Daniels-MacBook-Pro.local>
2017-04-02 22:28:59 -0500

    added reset.css and restyled

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,14 @@
-// 2. The player will have to guess the answer, just like in Hangman. This time, though, the player will guess with numbers instead of letters.
-//
-// 3. Here's how the app works:
-//
-//    * There will be four crystals displayed as buttons on the page.
-//
-//    * The player will be shown a random number at the start of the game.
-//
-//    * When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-//
-//      * Your game will hide this amount until the player clicks a crystal.
-//      * When they do click one, update the player's score counter.
-//
-//    * The player wins if their total score matches the random number from the beginning of the game.
-//
-//    * The player loses if their score goes above the random number.
-//
-//    * The game restarts whenever the player wins or loses.
-//
-//      * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-//
-//    * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-//
-// ##### Option 1 Game design notes
-//
-// * The random number shown at the start of the game should be between 19 - 120.
-//
-// * Each crystal should have a random hidden value between 1 - 12.
-
-var winCounter = 0;
-var lossCounter = 0;
-var scoreCounter = 0;
-
-var compRandomNum;
-var blueNum;
-var greenNum;
-var purpleNum;
-var redNum;
-
 $(document).ready(function() {
+
+  var winCounter = 0;
+  var lossCounter = 0;
+  var scoreCounter = 0;
+
+  var compRandomNum;
+  var blueNum;
+  var greenNum;
+  var purpleNum;
+  var redNum;
 
 function startGame() {
   //sets all random numbers for the round
@@ -49,7 +20,7 @@ function startGame() {
   scoreCounter = 0;
 
   //displays computer random number and scoreboard
-  $("#win-lose").html("CHOOSE WISELY");
+  $("#win-lose").html("");
   $("#random-num").html(compRandomNum);
   $("#win-count").html(winCounter);
   $("#loss-count").html(lossCounter);
@@ -61,6 +32,7 @@ function startGame() {
 function clickCrystals() {
   //Click events for each crystal
   $("#blue-crystal").on("click", function() {
+    //adds crystal value to round score count
     scoreCounter += blueNum;
     gameCheck();
   });
@@ -78,24 +50,22 @@ function clickCrystals() {
   });
 }
 
-
-
+//compares round score to the computer number
 function gameCheck() {
   console.log("Score Counter: " + scoreCounter);
   $("#round-points").html(scoreCounter);
 
   if (scoreCounter === compRandomNum){
-    $("#win-lose").html("You Win!");
+    $("#win-lose").html("YOU CHOSE WISELY!");
     winCounter++;
-    setTimeout(startGame, 2000);
+    setTimeout(startGame, 2500);
   } else if(scoreCounter > compRandomNum) {
-    $("#win-lose").html("You Lose!");
+    $("#win-lose").html("YOU CHOSE POORLY!");
     lossCounter++;
-    setTimeout(startGame, 2000);
+    setTimeout(startGame, 2500);
   };
 
 }
-
 
 
 clickCrystals();
